dd6b711ebc7f2f9d9f30c50b7940d4bda05cfa38
edwin <edwinvillabona0701@gmail.com>
2025-04-23 14:40:02 -0500

    creacion de condiciones

--- 3-eventos/app.js
+++ 3-eventos/app.js
@@ -1,7 +1,7 @@
 
 
 
-//variables
+//------------variables-------------------
 // let btn = document.querySelector("#btn_validar");
 const formulario = document.querySelector("form");
 const nombre = document.querySelector('[name="nombre"]');
@@ -10,14 +10,36 @@ const telefono = document.querySelector('[name="telefono"]');
 const documento = document.querySelector('[name="documento"]');
 const usuario = document.querySelector('[name="usuario"]');
 const contasena = document.querySelector('[name="contasena"]');
-//funciones
-//eventos
+//----------------funciones---------------------------
+
+//----------------eventos-------------------------------
 const validar = (event) => {
   event.preventDefault()
   if (nombre.value == "") {
     alert("ingrese un nombre");
     nombre.focus();
   }
+  else if (apellido.value == "") {
+    alert("ingrese un apellido");
+    apellido.focus();
+  }
+  else if (telefono.value == "") {
+    alert("ingrese un telefono");
+    telefono.focus();
+  }
+  else if (documento.value == "") {
+    alert("ingrese un documento");
+    documento.focus();
+  }
+  else if (usuario.value == "") {
+    alert("ingrese un usuario");
+    usuario.focus();
+  }
+  else if (contasena.value == "") {
+    alert("ingrese un contraseña");
+    contasena.focus();
+  }
+
 }
 formulario.addEventListener("submit", validar);
 
